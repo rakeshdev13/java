8cd47926587725069779ca01626d97714ab48876
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-11-14 23:31:54 +0530

    Update SumusingReturn.java

--- SumusingReturn.java
+++ SumusingReturn.java
@@ -3,12 +3,12 @@ package AllProgram;
 public class SumusingReturn {
 	
 	
-	 static int Addition(int a, int b) {
+	 static int addition(int a, int b) {
 		return(a+b);
 	}
 
 	public static void main(String[] args) {
-        int Sum=Addition(5,5);
+        int Sum=addition(5,5);
         System.out.println(Sum);
        
 	}
