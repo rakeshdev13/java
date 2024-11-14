b961be06c7d01e2176833b0b62e3c14d8e69c228
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-11-14 18:36:56 +0530

    Add files via upload

---
+++ MethodSample.java
@@ -0,0 +1,12 @@
+package AllProgram;
+
+public class MethodSample {
+	static void method(){
+		
+		System.out.print("Hi I'm Rakesh");	
+	}
+	public static void main(String[] args) {
+		method(); //calling a method
+	}
+
+}
