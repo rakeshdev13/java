e33a3278912963b60fe4f7152607aa0c54c37eb1
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-11-14 23:30:27 +0530

    Add files via upload

---
+++ SumusingMethod.java
@@ -0,0 +1,13 @@
+package AllProgram;
+
+public class SumusingMethod {
+	
+	static void Sum(int a,int b) {
+		System.out.println(a+b);
+	}
+	
+	public static void main(String[] args) {
+		Sum(1,2);
+	}
+
+}
---
+++ SumusingReturn.java
@@ -0,0 +1,16 @@
+package AllProgram;
+
+public class SumusingReturn {
+	
+	
+	 static int Addition(int a, int b) {
+		return(a+b);
+	}
+
+	public static void main(String[] args) {
+        int Sum=Addition(5,5);
+        System.out.println(Sum);
+       
+	}
+
+}
