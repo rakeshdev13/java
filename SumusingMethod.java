9ded4e29f2ffe91544668f33cc2c5996920134a9
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-11-14 23:31:11 +0530

    Update SumusingMethod.java

--- SumusingMethod.java
+++ SumusingMethod.java
@@ -2,12 +2,12 @@ package AllProgram;
 
 public class SumusingMethod {
 	
-	static void Sum(int a,int b) {
+	static void sum(int a,int b) {
 		System.out.println(a+b);
 	}
 	
 	public static void main(String[] args) {
-		Sum(1,2);
+		sum(1,2);
 	}
 
 }
