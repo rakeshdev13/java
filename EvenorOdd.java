f0d27adcc32a4245facb39ae2305aeede499f285
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-11-19 17:11:02 +0530

    Add files via upload

---
+++ EvenorOdd.java
@@ -0,0 +1,17 @@
+package AllProgram;
+
+public class EvenorOdd {
+
+	public static void main(String[] args) {
+		// print Even or odd seperately using if 
+		
+		int num=10;
+		if(num%2==0) {
+			System.out.println("It is an Even number");
+		}
+		if(num%2==1) {
+			System.out.println("It is an Odd number");
+		}
+	}
+
+}
---
+++ WhileloopTask.java
@@ -0,0 +1,15 @@
+package AllProgram;
+
+public class WhileloopTask {
+
+	public static void main(String[] args) {
+		int i =10;
+		
+		while(i<=100) {
+			System.out.println(i);
+			i=i+10; // incrementing 
+		}
+
+	}
+
+}
