4438b09d8758a8c6ab393dbc819ddd1a09ae6f3b
rakeshdev13 <rakeshdev.rbk@gmail.com>
2024-12-09 21:57:26 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,43 @@
+'''Programming Language:
+A programming language is a formal system used to communicate instructions to a computer.
+It consists of a set of syntax rules and semantics that enable developers to write programs that a computer can understand and execute.
+These languages allow humans to create software, control hardware, process data, and perform other tasks.
+
+2)List of the programming language:
+Python
+Java
+JavaScript
+C
+C++
+Ruby
+
+3)LIST                                   TUPLE
+* LIST denoted by []                     Tuple is denoted by ()
+*List is collection of MTD               Tupleis collection of MTD
+*It allows duplicates                    *It allows duplicates
+*It's mutable                             *It's immutable
+*List is ordered and indexed              *List is ordered and indexed
+
+ 4)mylist = [1, 2, 3, "apple"]
+Functionalities:
+Access elements via indexing (my_list[0])
+Modify elements (my_list[1] = "banana")
+Add elements using .append(), .insert()
+Remove elements using .remove(), .pop(), .clear()
+Sorting with .sort() and reversing with .reverse()
+List slicing (my_list[1:3])
+List comprehensions
+
+
+5)mytuple = (1, 2, 3, "apple")
+Functionalities:
+Access elements via indexing (my_tuple[0])
+Iterate over tuple elements
+Can be used for multiple assignments (a, b = (1, 2))
+Tuple unpacking
+
+6)my_set = {1, 2, 3, "apple"}
+Functionalities:
+Add elements with .add()
+Remove elements with .remove(), .discard()'''
+
